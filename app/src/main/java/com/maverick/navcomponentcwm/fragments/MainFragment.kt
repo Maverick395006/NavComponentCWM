b1f5e63d22d652b8fb8ca39c808c87adedb4343f
Maverick395006 <87902602+Maverick395006@users.noreply.github.com>
2022-12-12 09:28:19 +0530

    OnClick setup in MainFragment.kt (Using OnClickListener Interface)

--- app/src/main/java/com/maverick/navcomponentcwm/fragments/MainFragment.kt
+++ app/src/main/java/com/maverick/navcomponentcwm/fragments/MainFragment.kt
@@ -5,13 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.maverick.navcomponentcwm.R
 import com.maverick.navcomponentcwm.databinding.FragmentMainBinding
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(), View.OnClickListener {
 
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
 
+    var navController: NavController? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,6 +27,27 @@ class MainFragment : Fragment() {
 
 
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+
+        binding.apply {
+            viewTransactionsBtn.setOnClickListener(this@MainFragment)
+            sendMoneyBtn.setOnClickListener(this@MainFragment)
+            viewBalanceBtn.setOnClickListener(this@MainFragment)
+        }
+
+    }
+
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            binding.viewTransactionsBtn.id -> navController!!.navigate(R.id.action_mainFragment_to_viewTransactionFragment)
+            binding.sendMoneyBtn.id -> navController!!.navigate(R.id.action_mainFragment_to_chooseRecipientFragment)
+            binding.viewBalanceBtn.id -> navController!!.navigate(R.id.action_mainFragment_to_viewBalanceFragment)
+        }
     }
 
     override fun onDestroyView() {
