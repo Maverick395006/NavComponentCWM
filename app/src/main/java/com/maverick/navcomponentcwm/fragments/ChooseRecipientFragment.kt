755b4d994d032d78c504d6d3177cca75fa9c4513
Maverick395006 <87902602+Maverick395006@users.noreply.github.com>
2022-12-13 12:43:09 +0530

    Animation And Transition Added.

--- app/src/main/java/com/maverick/navcomponentcwm/fragments/ChooseRecipientFragment.kt
+++ app/src/main/java/com/maverick/navcomponentcwm/fragments/ChooseRecipientFragment.kt
@@ -5,13 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.maverick.navcomponentcwm.R
 import com.maverick.navcomponentcwm.databinding.FragmentChooseReceipientBinding
 
-class ChooseRecipientFragment : Fragment() {
+class ChooseRecipientFragment : Fragment(), View.OnClickListener {
 
     private var _binding: FragmentChooseReceipientBinding? = null
     private val binding get() = _binding!!
 
+    var navController: NavController? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,8 +29,30 @@ class ChooseRecipientFragment : Fragment() {
         return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+
+        binding.apply {
+            nextBtn.setOnClickListener(this@ChooseRecipientFragment)
+            cancelBtn.setOnClickListener(this@ChooseRecipientFragment)
+        }
+
+    }
+
+    override fun onClick(v: View?) {
+        binding.apply {
+            when (v!!.id) {
+                nextBtn.id -> navController!!.navigate(R.id.action_chooseRecipientFragment_to_specifyAmountFragment)
+                cancelBtn.id -> activity?.onBackPressed()
+            }
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+        navController = null
     }
 }
